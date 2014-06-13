46576de9d460f29baf0a8b392612907b3a2a55eb
Guilherme Pontes <guisantos@sp.r7.com>
2014-06-13 15:46:29 -0300

    adding karma and structure the file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,28 @@
-var gulp = require('gulp');
-var jasmine = require('gulp-jasmine');
+var gulp  = require('gulp');
+var karma = require('gulp-karma');
 
-gulp.task('default', function () {
-	return gulp.src('spec/*.js').pipe( jasmine() );
-});
+var GulpApp = {
+	testFiles: [
+	  'src/*.js',
+	  'spec/*.js'
+	],
+
+	action: "run",
+
+	test: function() {
+		return gulp.src(GulpApp.testFiles).pipe(
+			karma({
+				configFile: 'karma.conf.js',
+				action: GulpApp.action
+			})
+		);
+	},
+
+	default: function() {
+		GulpApp.action = "watch";
+		return GulpApp.test();
+	}
+};
+
+gulp.task('test', GulpApp.test);
+gulp.task('default', GulpApp.default);
