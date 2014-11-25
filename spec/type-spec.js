35cde159bba6d83c3851af013a39422f949d826d
Daniel Paulino <danielpaulino030@gmail.com>
2014-11-25 19:51:28 -0200

    Fix typo on tests

--- spec/type-spec.js
+++ spec/type-spec.js
@@ -5,19 +5,18 @@ describe("[[R7Storage]]", function() {
       store = R7Storage;
     });
 
+    it("must the type() be empty", function() {
+      expect(store.type).toBe("");
+    });
+
     it("Must the type() be cookies", function() {
       R7Storage.use("cookies");
       expect(R7Storage.type).toBe("cookies");
     });
 
-    it("must the type() be localstorage", function() {
-      R7Storage.use("localstorage");
-      expect(R7Storage.type).toBe("localstorage");
-    });
-
-    it("must the type() be empty", function() {
-      console.log(store)
-      expect(store.type).toBe("");
+    it("must the type() be localStorage", function() {
+      R7Storage.use("localStorage");
+      expect(R7Storage.type).toBe("localStorage");
     });
   });
 });
