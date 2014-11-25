38de08b12f2d39ad303f5bbd362fca6bde3812f7
Daniel Paulino <danielpaulino030@gmail.com>
2014-11-25 19:55:58 -0200

    Removing unnecessary beforeEach

--- spec/type-spec.js
+++ spec/type-spec.js
@@ -1,22 +1,19 @@
 describe("[[R7Storage]]", function() {
   describe("[Setting technology type]", function() {
-    var store;
-    beforeEach(function(){
-      store = R7Storage;
-    });
+    var store = R7Storage;
 
     it("must the type() be empty", function() {
       expect(store.type).toBe("");
     });
 
     it("Must the type() be cookies", function() {
-      R7Storage.use("cookies");
-      expect(R7Storage.type).toBe("cookies");
+      store.use("cookies");
+      expect(store.type).toBe("cookies");
     });
 
     it("must the type() be localStorage", function() {
-      R7Storage.use("localStorage");
-      expect(R7Storage.type).toBe("localStorage");
+      store.use("localStorage");
+      expect(store.type).toBe("localStorage");
     });
   });
 });
